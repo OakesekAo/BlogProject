fc79ffeceba63fdfffb3cef44c653a075f4907a4
OakeseskAo <oakes.a@live.com>
2022-12-13 21:48:41 -0500

    Add project files.

---
+++ Models/Blog.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace BlogProject.Models
+{
+    public class Blog
+    {
+        //Id of the blog entery
+        public int Id { get; set; }
+        //author of the entry
+        public string AuthorId { get; set; }
+        //Title/Description entry
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //Date blog was created
+        public DateTime Created { get; set; }
+        //Date blog was updated/edited - can be nullable
+        public DateTime? Updated { get; set; }
+
+        //byte array for storing image data
+        public byte[] ImageData { get; set; }
+        // storing type of file so we can convert back to an image
+        public string ContentType { get; set; }
+        public IFormFile Image { get; set; }
+
+    }
+}
