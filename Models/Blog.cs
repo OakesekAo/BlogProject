d4e06eb9e1eccd9b3c233968b3a9c5020aba28e2
OakeseskAo <oakes.a@live.com>
2022-12-15 11:43:18 -0500

    Updated Blog Model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlogProject.Models
 {
@@ -10,17 +12,33 @@ namespace BlogProject.Models
         //author of the entry
         public string AuthorId { get; set; }
         //Title/Description entry
+        [Required]
+        //passing an error message with 0 = name of variable, 2 = minimumLength value, 1 = 100
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
         public string Name { get; set; }
+        [Required]
+        //passing an error message with 0 = name of variable, 2 = minimumLength value, 1 = 100
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
         public string Description { get; set; }
+
         //Date blog was created
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
         public DateTime Created { get; set; }
+
         //Date blog was updated/edited - can be nullable
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
         public DateTime? Updated { get; set; }
 
         //byte array for storing image data
+        [Display(Name = "Blog Image")]
         public byte[] ImageData { get; set; }
         // storing type of file so we can convert back to an image
+        [Display(Name = "Image Type")]
         public string ContentType { get; set; }
+
+        [NotMapped]
         public IFormFile Image { get; set; }
 
     }
