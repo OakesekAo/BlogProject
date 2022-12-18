e9071636cdaa14dbac452efee6c645a0d72c5d0f
OakeseskAo <oakes.a@live.com>
2022-12-17 22:26:12 -0500

    Update BlogUser.cs
    missed close pracket

--- Models/BlogUser.cs
+++ Models/BlogUser.cs
@@ -12,15 +12,15 @@ namespace BlogProject.Models
         [Display(Name = "Fist Name")]
         public string FirstName { get; set; }
         [Required]
-        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
         [Display(Name = "Fist Name")]
         public string LastName { get; set; }
         public byte[] ImageData { get; set; }
         public string ContentType { get; set; }
 
-        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
         public string FacebookUrl { get; set; }
-        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
         public string TwitterUrl { get; set; }
         [NotMapped]
         public string FullName
