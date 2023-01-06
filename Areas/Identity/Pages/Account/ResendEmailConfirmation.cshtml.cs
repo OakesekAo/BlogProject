50df8faeb798173aff439c169baaa57d5feec681
OakeseskAo <oakes.a@live.com>
2023-01-05 21:54:34 -0500

    Updated LoginPartial
    Fixed the displaying name for login partial after a user logs in

--- Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using BlogProject.Models;
 using BlogProject.Services;
 
 namespace BlogProject.Areas.Identity.Pages.Account
